fe092df1bfeb706b3f08a48cb394ea0a2a282616
Damian <37978110+dszyszek@users.noreply.github.com>
2018-12-26 13:42:44 +0100

    Basic setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use('/', express.static(path.join(__dirname + '/../' + '/public')));
+
+
+console.log(port);
+
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+});
