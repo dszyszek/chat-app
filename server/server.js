dba1cc688b7e8bc8532a1cec99278ffb843cd6d6
Damian <37978110+dszyszek@users.noreply.github.com>
2018-12-26 14:38:46 +0100

    Add socket.io and Front-End script file

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const socketio = require('socket.io');
+
 const path = require('path');
+const http = require('http');
+
+
+const port = process.env.port || 3000;
 
 const app = express();
-const port = process.env.port || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use('/', express.static(path.join(__dirname + '/../' + '/public')));
 
 
 console.log(port);
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App started on port ${port}`);
 });
