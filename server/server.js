e5145972379c3f3e04d31d7c152a72f0bf279136
Damian <37978110+dszyszek@users.noreply.github.com>
2018-12-29 03:21:36 +0100

    Update PORT variable

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const http = require('http');
 
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
