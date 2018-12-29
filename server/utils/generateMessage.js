0f620f33c15fcc4f99870004511becf36efb32ad
Damian <37978110+dszyszek@users.noreply.github.com>
2018-12-29 17:11:04 +0100

    Fix problem with Date().getMonth()

--- server/utils/generateMessage.js
+++ server/utils/generateMessage.js
@@ -1,6 +1,6 @@
 const generateMessage = (message) => {
     let time = new Date();
-        let formattedTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}, ${time.getDate()}.${time.getMonth()}.${time.getFullYear()}`;
+        let formattedTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}, ${time.getDate()}.${time.getMonth()+1}.${time.getFullYear()}`;
 
         let newMessage = {
             from: message.from,
